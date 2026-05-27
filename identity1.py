695d23b64d4a16caf0abb154a4f8345dc5746d62
headshotive <aarav.nator@gmail.com>
2026-05-27 19:41:42 +0800

    Add files via upload

---
+++ identity1.py
@@ -0,0 +1,14 @@
+x=6
+if (type(x) is int):
+  print("true")
+else:
+  print("false")
+x=5.5
+y=5.5
+if (type(x)is not float):
+  print("true")
+else:
+  print("false")
+if (x is y):
+  print("x and y same identity")
+
