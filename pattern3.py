669383e1bf8df1304407716943dc6643d87995fb
headshotive <aarav.nator@gmail.com>
2026-07-01 19:39:17 +0800

    Add files via upload

---
+++ pattern3.py
@@ -0,0 +1,13 @@
+n=int(input("enter the number of rows:"))
+for i in range(1,n+1,2):
+	spaces=(n-i)//2
+	print(" "*spaces,end=" ")
+	for j in range(1,i+1):
+		print(j,end=" ")
+	print()
+for i in range(n-2,0,-2):
+	spaces=(n-i)//2
+	print(" "*spaces,end=" ")
+	for j in range(1,i+1):
+		print(j,end=" ")
+	print()
